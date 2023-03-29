373358b9aca7ee09bd7b34faa444ea1b50eeb0fa
Sydney Hopson <sydneyhopsonreignmobile@Sydneys-iMac.local>
2023-03-28 20:41:02 -0400

    addOne

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,13 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 import reducer,{ initialState } from '/src/reducers'
+import { addOne } from '../actions';
 
 function App() {
 const [ state, dispatch] = useReducer(reducer, initialState)
-
+const handle1Click = () => {
+  dispatch(addOne());
+}
 
   return (
     <div className="App">
@@ -34,7 +37,7 @@ const [ state, dispatch] = useReducer(reducer, initialState)
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handle1Click} />
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
