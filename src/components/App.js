11cc77b7782030b96572b3d3b2f83ddd72b2532c
Sydney Hopson <sydneyhopsonreignmobile@Sydneys-iMac.local>
2023-03-30 16:58:35 -0400

    added clear button

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 import reducer,{ initialState } from '/src/reducers'
-import { applyNumber, changeOperation } from '../actions';
+import { applyNumber, changeOperation, clearDisplay } from '../actions';
 
 function App() {
 const [ state, dispatch] = useReducer(reducer, initialState)
@@ -15,6 +15,10 @@ const handleNumberClick = (number) => {
 }
 const handleOperationClick = (operator) => {
   dispatch(changeOperation(operator));
+}
+
+const handleClearDisplay = () => {
+  dispatch(clearDisplay());
 }
   return (
     <div className="App">
@@ -63,7 +67,7 @@ const handleOperationClick = (operator) => {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={handleClearDisplay }/>
             </div>
 
           </form>
